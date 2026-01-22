b8d642ddf6ab2c285401bd5263cc3454c6b3e60b
nenoeldeeb <69523673+Nenoeldeeb@users.noreply.github.com>
2026-01-22 21:32:37 +0200

    Add a better way to provide a viewModels in manual DI.
    Signed-off-by: nenoeldeeb <69523673+Nenoeldeeb@users.noreply.github.com>

--- app/src/main/java/dev/nenoeldeeb/education/absencerecord/app/MainActivity.kt
+++ app/src/main/java/dev/nenoeldeeb/education/absencerecord/app/MainActivity.kt
@@ -10,8 +10,6 @@ import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.lifecycle.viewmodel.initializer
-import androidx.lifecycle.viewmodel.viewModelFactory
 import dev.nenoeldeeb.education.absencerecord.presentation.screens.calendar.CalendarScreen
 import dev.nenoeldeeb.education.absencerecord.presentation.screens.calendar.CalendarViewModel
 import dev.nenoeldeeb.education.absencerecord.presentation.screens.report.ReportScreen
@@ -24,49 +22,12 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val app = (application as Application)
-            val studentsViewModel: StudentsViewModel =
-                viewModel(
-                    factory =
-                        viewModelFactory {
-                            initializer {
-                                StudentsViewModel(
-                                    studentManagementUseCases = app.appContainer.studentManagementUseCases
-                                )
-                            }
-                        }
-                )
-            val calendarViewModel: CalendarViewModel =
-                viewModel(
-                    factory =
-                        viewModelFactory {
-                            initializer {
-                                CalendarViewModel(
-                                    attendanceUseCases = app.appContainer.attendanceUseCases,
-                                    studentManagementUseCases = app.appContainer.studentManagementUseCases
-                                )
-                            }
-                        }
-                )
-            val reportViewModel: ReportViewModel =
-                viewModel(
-                    factory =
-                        viewModelFactory {
-                            initializer {
-                                ReportViewModel(
-                                    studentManagementUseCases = app.appContainer.studentManagementUseCases,
-                                    attendanceUseCases = app.appContainer.attendanceUseCases,
-                                    reportUseCases = app.appContainer.reportUseCases
-                                )
-                            }
-                        }
-                )
             AbsenceRecordTheme {
                 Surface {
                     MainScreen(
-                        studentsViewModel = studentsViewModel,
-                        calendarViewModel = calendarViewModel,
-                        reportViewModel = reportViewModel
+                        studentsViewModel = viewModel(factory = AppViewModelProvider.factory),
+                        calendarViewModel = viewModel(factory = AppViewModelProvider.factory),
+                        reportViewModel = viewModel(factory = AppViewModelProvider.factory)
                     )
                 }
             }
