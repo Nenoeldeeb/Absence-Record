4c960950b874c209f037c28e3462102be4332a67
nenoeldeeb <69523673+Nenoeldeeb@users.noreply.github.com>
2026-03-03 21:33:20 +0200

    Implement a new feature called "Classes" which organize the students in the same group.
    Signed-off-by: nenoeldeeb <69523673+Nenoeldeeb@users.noreply.github.com>

--- app/src/main/java/dev/nenoeldeeb/education/absencerecord/presentation/screens/students/dialogs/StudentDialog.kt
+++ app/src/main/java/dev/nenoeldeeb/education/absencerecord/presentation/screens/students/dialogs/StudentDialog.kt
@@ -10,11 +10,11 @@ import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuAnchorType
 import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.MenuAnchorType
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
@@ -98,7 +98,7 @@ internal fun StudentDialog(
                                 )
                             },
                             modifier = Modifier
-                                .menuAnchor(MenuAnchorType.PrimaryNotEditable)
+                                .menuAnchor(ExposedDropdownMenuAnchorType.PrimaryNotEditable)
                                 .fillMaxWidth()
                         )
                         ExposedDropdownMenu(
