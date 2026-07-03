b9d113d0525db46eb5131ed75f12eff147750b18
nenoeldeeb <69523673+Nenoeldeeb@users.noreply.github.com>
2026-07-04 01:34:20 +0300

    fix: remove trailing newlines in multiple test files
    Signed-off-by: nenoeldeeb <69523673+Nenoeldeeb@users.noreply.github.com>

--- app/src/androidTest/java/dev/nenoeldeeb/education/absencerecord/presentation/screens/calendar/CalendarScreenTest.kt
+++ app/src/androidTest/java/dev/nenoeldeeb/education/absencerecord/presentation/screens/calendar/CalendarScreenTest.kt
@@ -1,10 +1,8 @@
 package dev.nenoeldeeb.education.absencerecord.presentation.screens.calendar
 
 import androidx.compose.ui.test.assertIsDisplayed
-import androidx.compose.ui.test.hasText
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onAllNodesWithContentDescription
-import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
 import androidx.test.ext.junit.runners.AndroidJUnit4
